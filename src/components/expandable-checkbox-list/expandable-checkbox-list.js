8470690dc361b8109c58557f344d5235b03d105e
Igor Smolkov <smolkov.igor@gmail.com>
2020-10-05 14:26:41 +0700

    Подправил раскрывающийся чекбокс-список

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,9 +1,12 @@
 export default function() {
     const expandableCheckboxLists = document.querySelectorAll('.expandable-checkbox-list');
     expandableCheckboxLists.forEach(expandableCheckboxList => {
-        console.log(expandableCheckboxList.previousSibling);
         expandableCheckboxList.previousSibling.addEventListener('click', function(){
-            console.log(expandableCheckboxList.lastChild.className);
+            if ((expandableCheckboxList.lastChild) && (expandableCheckboxList.lastChild.className === 'expandable-checkbox-list__item')){
+                expandableCheckboxList.classList.toggle('expandable-checkbox-list_expanded');
+            }
+        });
+        expandableCheckboxList.addEventListener('click', function(){
             if ((expandableCheckboxList.lastChild) && (expandableCheckboxList.lastChild.className === 'expandable-checkbox-list__item')){
                 expandableCheckboxList.classList.toggle('expandable-checkbox-list_expanded');
             }
