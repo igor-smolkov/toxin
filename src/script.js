e6db41d961f46ecb1faa9ce6fc4cf88595f521ba
Igor Smolkov <smolkov.igor@gmail.com>
2021-03-04 20:54:05 +0700

    Убрал миксы

--- src/script.js
+++ src/script.js
@@ -19,7 +19,6 @@ menu()
 
 // import '@comp/colors-style/colors-style.scss'
 import '@comp/type-style/type-style.scss'
-import '@comp/logo-zone/logo-zone.scss'
 import '@comp/logo/logo.scss'
 import '@comp/color-section/color-section.scss'
 import '@comp/type-section/type-section.scss'
