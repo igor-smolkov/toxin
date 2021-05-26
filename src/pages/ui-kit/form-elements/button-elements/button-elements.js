00efd026e7ea013100aa3b4068b5d5e8438d60c7
Igor Smolkov <smolkov.igor@gmail.com>
2021-05-26 16:18:29 +0700

    Подкорректировал импорты

--- src/pages/ui-kit/form-elements/button-elements/button-elements.js
+++ src/pages/ui-kit/form-elements/button-elements/button-elements.js
@@ -5,6 +5,4 @@ import '@comp/form-set/form-set'
 
 import '@comp/slider/slider'
 import '@comp/button/button'
-
-import '@comp/btn/btn.scss'
-import '@comp/pagination/pagination.scss'
+import '@comp/pagination/pagination'
