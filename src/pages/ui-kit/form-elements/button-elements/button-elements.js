e75273bf20dd30f12ab7365ad8939db18892e6eb
Igor Smolkov <smolkov.igor@gmail.com>
2021-05-26 16:16:05 +0700

    Отрефакторил кнопочные элементы

--- src/pages/ui-kit/form-elements/button-elements/button-elements.js
+++ src/pages/ui-kit/form-elements/button-elements/button-elements.js
@@ -1,9 +1,10 @@
 import './button-elements.scss';
 
-import '@comp/form-section/form-section.scss'
-import '@comp/form-set/form-set.scss'
+import '@comp/form-section/form-section'
+import '@comp/form-set/form-set'
 
 import '@comp/slider/slider'
+import '@comp/button/button'
 
 import '@comp/btn/btn.scss'
 import '@comp/pagination/pagination.scss'
