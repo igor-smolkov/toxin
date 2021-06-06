41cd9cada2bd212419316398cf5bef39e2c8718b
Igor Smolkov <smolkov.igor@gmail.com>
2021-06-06 20:41:03 +0700

    Переделал дропдауны

--- src/pages/ui-kit/form-elements/form-elements.js
+++ src/pages/ui-kit/form-elements/form-elements.js
@@ -4,4 +4,6 @@ import './field-elements/field-elements';
 import './check-elements/check-elements';
 import './button-elements/button-elements';
 
+import './dropdowns/dropdowns';
+
 import '@comp/logo/logo.scss'
