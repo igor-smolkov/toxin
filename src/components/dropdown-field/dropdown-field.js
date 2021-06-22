d1be04d6dc8d2e362dedf85b3138604051a9a517
Igor Smolkov <smolkov.igor@gmail.com>
2021-06-22 12:05:50 +0700

    Отрефакторил дропдаун, добавил сброс значений

--- src/components/dropdown-field/dropdown-field.js
+++ src/components/dropdown-field/dropdown-field.js
@@ -1,23 +1,35 @@
 import './dropdown-field.scss'
 
 class Counter {
-  constructor($elem, dropdown) {
+  constructor($elem) {
     this._$elem = $elem;
-    this._dropdown = dropdown;
+
     this._$minusButton = this._findMinusButton();
     this._$plusButton = this._findPlusButton();
     this._$countField = this._findCountField();
 
-    this.count = +this._$countField.text();
+    this._count = +this._$countField.text();
 
-    this._$minusButton.on('click', (e) => this._handleMinusButtonClick(e));
-    this._$plusButton.on('click', (e) => this._handlePlusButtonClick(e));
+    this._listenControls();
+  }
+  setCount(value) {
+    this._count = value;
+    this._updateCountField();
+    this.disabledMinusButton(this._count === 0);
+  }
+  getCount() {
+    return this.count
   }
   decrementCount() {
-    this.setCount(this.count-1);
+    this.setCount(this._count-1);
   }
   incrementCount() {
-    this.setCount(this.count+1);
+    this.setCount(this._count+1);
+  }
+  reset() {
+    this._count = 0;
+    this._updateCountField();
+    this.disabledMinusButton(true);
   }
   disabledMinusButton(isTrue = true) {
     if (isTrue) {
@@ -28,146 +40,56 @@ class Counter {
     this._$minusButton.prop('disabled', isTrue);
   }
 
-  setCount(value) {
-    this.count = value;
-    this._updateCountField();
-    this.disabledMinusButton(this.count === 0);
+  _findMinusButton() {
+    return $(this._$elem.find('.dropdown-counter__button')[0]);
   }
-  getCount() {
-    return this.count
+  _findPlusButton() {
+    return $(this._$elem.find('.dropdown-counter__button')[1]);
   }
-  reset() {
-    this.count = 0;
-    this._updateCountField();
-    this.disabledMinusButton(true);
+  _findCountField() {
+    return $(this._$elem.find('.dropdown-counter__count'));
   }
-
+  _listenControls() {
+    this._$minusButton.on('click', (e) => this._handleMinusButtonClick(e));
+    this._$plusButton.on('click', (e) => this._handlePlusButtonClick(e));
+  } 
   _handleMinusButtonClick(e) {
     e.preventDefault();
     this.decrementCount();
-    // this._dropdown._handlePlusMinusButton(false);
   }
   _handlePlusButtonClick(e) {
     e.preventDefault();
     this.incrementCount();
-    // this._dropdown._handlePlusMinusButton(true);
   }
   _updateCountField() {
-    this._$countField.text(this.count.toString());
-  }
-  _findMinusButton() {
-    return this._findButtonByIndex(0);
-  }
-  _findPlusButton() {
-    return this._findButtonByIndex(1);
-  }
-  _findButtonByIndex(index) {
-    return $(this._$elem.find('.dropdown-counter__button')[index]);
-  }
-  _findCountField() {
-    return $(this._$elem.find('.dropdown-counter__count'));
+    this._$countField.text(this._count.toString());
   }
 }
 
 class Dropdown {
   constructor($elem) {
     this._$elem = $elem;
+    this._id = this._$elem.attr('id');
     this._counters = this._initCounters();
-
-    this._$textField = this._findTextField();
-    this._$textField.on('input', (e)=>this._handleTextFieldInput(e));
-    this._list = this._initList();
+    this._$clearButton = $(`#${this._id}-clear`);
+    this._$clearButton.on('click', (e) => this._handleClearButtonClick(e));
   }
   _initCounters() {
     const counters = [];
     this._$elem.find('.dropdown-counter').each((_, counter) => {
-      counters.push(new Counter($(counter), this));
+      counters.push(new Counter($(counter)));
     })
     return counters;
   }
-
-  resetCounters() {
-    this.counters.forEach(counter => {
+  _handleClearButtonClick(e) {
+    e.preventDefault();
+    this._resetCounters();
+  }
+  _resetCounters() {
+    this._counters.forEach(counter => {
       counter.reset();
     });
   }
-  _handleTextFieldInput(e) {
-    e.preventDefault();
-    const matchValues = e.target.value.match(/\d+/g);
-    let values = matchValues ? matchValues : this.resetCounters();
-    values.forEach((value, indexValue) => {
-      if (value < 100) {
-        const count = value - this.counters.reduce((acc, counter, indexCounter) => {
-          if (indexCounter <= indexValue) return
-          return acc += counter.getCount()
-        }, 0);
-        this.resetCounters();
-        this.counters.
-          filter((_, indexCounter) => indexCounter <= indexValue).
-          forEach(counter => counter.setCount(count > 0 ? count : value));
-      }
-    });
-  }
-  _handlePlusMinusButton(sign = true) {
-    const countSum = this._counters.reduce((acc, counter) => acc += counter.getCount(), 0)
-    if (countSum === 0 && !sign) {
-      this._$textField.val('');
-      this._$textField.attr('placeholder', this._isGuest() ? 'Сколько гостей' : '');
-    } else {
-      if (this._isGuest()) {
-        this._$textField.val(guestGrammar(countSum.toString()));
-      } else {
-        let value = '';
-        this._counters.forEach((counter, index) => {
-          if (index === this._counters.length-1) return;
-          value += `${guestGrammar(counter.getCount(), this._list[index])}, `
-        });
-        this._$textField.val(`${value}...`);
-      }
-    } 
-  }
-  _findTextField() {
-    return $(this._$elem.find('.text-field'));
-  }
-  _isGuest() {
-    return this._$textField.val().match(/гост/g) ? true : false;
-  }
-  _initList() {
-    const list = [];
-    this._$elem.find('.dropdown-item__name').each((_, item) => {
-      list.push($(item).text());
-    })
-    return list;
-  }
 }
 
-$('.dropdown').each((_, elem) => new Dropdown($(elem)))
-
-function guestGrammar(value, other) {
-  if (value[value.length-1].match(/0|[5-9]/g) || (value*1>10 && value*1<20)) {
-    if (other == 'спальни') {
-      value += ' спален'
-    } else if (other == 'кровати') {
-      value += ' кроватей'
-    } else {
-      value += ' гостей';
-    }
-  } else if (value[value.length-1].match(/1/g)) {
-    if (other == 'спальни') {
-      value += ' спаленя'
-    } else if (other == 'кровати') {
-      value += ' кровать'
-    } else {
-      value += ' гость';
-    }
-  } else {
-    if (other == 'спальни') {
-      value += ' спалени'
-    } else if (other == 'кровати') {
-      value += ' кровати'
-    } else {
-      value += ' гостя';
-    }
-  }
-  return value;
-}
+$('.dropdown').each((_, elem) => new Dropdown($(elem)));
