9079bdae9cd023647da378fb55530c024bdc9733
Igor Smolkov <smolkov.igor@gmail.com>
2021-07-29 21:27:34 +0700

    Доработал дропдаун

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -6,6 +6,7 @@ import '@comp/dropdown-control/dropdown-control';
 class Counter {
   constructor($elem) {
     this._$elem = $elem;
+    this.category = this._$elem.data().category
 
     this._$minusButton = this._findMinusButton();
     this._$plusButton = this._findPlusButton();
@@ -82,6 +83,7 @@ class Dropdown {
     this._$dropper = $(`#${this._id}-check`);
     this._$dropper.on('change', (e) => this._handleDrop(e));
     this._$elem.on('click', this._handleCheckClear.bind(this))
+    if (this._counters.length) this._updateField();
   }
   _initCounters() {
     const counters = [];
@@ -105,21 +107,60 @@ class Dropdown {
     this._updateField();
   }
   _updateField() {
-    this._$field.val(this._makeGuestStr(this._calcCountersSum()))
+    this._$field.val(this._makeFiledStr());
+  }
+  _makeFiledStr() {
+    let str = '';
+    let i = 0;
+    this._calcCountersSums().forEach((count, category) => {
+      if (count === 0) return;
+      str += i++ !== 0 ? ', ' : '';
+      switch(category) {
+        case 'гость':
+          str += this._makeWordFormStr(count, ['гость', 'гостя', 'гостей']);
+          break;
+        case 'младенец':
+          str += this._makeWordFormStr(count, ['младенец', 'младенеца', 'младенецев']);
+          break;
+        case 'спальня':
+          str += this._makeWordFormStr(count, ['спальня', 'спальни', 'спален'])
+          break;
+        case 'кровать':
+          str += this._makeWordFormStr(count, ['кровать', 'кровати', 'кроватей'])
+          break;
+        case 'ванная':
+          str += this._makeWordFormStr(count, ['ванная', 'ванные', 'ванных'])
+          break;
+        default:
+          str += this._makeWordFormStr(count, ['штука', 'штуки', 'штук'])
+      }
+    })
+    const isGuests = this._counters.find(counter => counter.category === 'гость');
+    if (!str) { str = isGuests ? 'Сколько гостей' : 'Сколько нужно' }
+    if (str.length > 19 && !this._$applyButton[0]) {str = `${str.substr(0, 20)}...`}
+    return str;
+  }
+  _calcCountersSums() {
+    const categories = new Set();
+    this._counters.forEach(counter => categories.add(counter.category));
+    const sums = new Map();
+    categories.forEach(category => {
+      sums.set(category, this._counters.reduce((sum, counter) => (counter.category === category ? sum + counter.getCount() : sum), 0));
+    })
+    return sums;
   }
   _calcCountersSum() {
     return this._counters.reduce((sum, counter) => sum + counter.getCount(), 0);
   }
-  _makeGuestStr(count) {
-    if (count === 0) return 'Сколько гостей';
+  _makeWordFormStr(count, forms) {
     if (count > 10) {
       const lastTwoDigits = +count.toString().substring(count.toString().length-2);
-      if (lastTwoDigits > 10 && lastTwoDigits <= 19) return `${count} гостей`;
+      if (lastTwoDigits > 10 && lastTwoDigits <= 19) return `${count} ${forms[2]}`;
     }
     const lastDigit = +count.toString()[count.toString().length-1];
-    if (lastDigit === 1) return `${count} гость`;
-    if (lastDigit > 1 && lastDigit <= 4) return `${count} гостя`;
-    if (lastDigit > 4 && lastDigit <= 9 || lastDigit === 0) return `${count} гостей`;
+    if (lastDigit === 1) return `${count} ${forms[0]}`;
+    if (lastDigit > 1 && lastDigit <= 4) return `${count} ${forms[1]}`;
+    if (lastDigit > 4 && lastDigit <= 9 || lastDigit === 0) return `${count} ${forms[2]}`;
   }
   _handleDrop(e) {
     if (e.target.checked) {
@@ -133,6 +174,9 @@ class Dropdown {
       this._$clearButton.removeClass('button_none')
     } else {
       this._$clearButton.addClass('button_none')
+    }
+    if(!this._$applyButton[0] && this._counters.length) {
+      this._updateField();
     }
   }
 }
--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -2,3 +2,4 @@ import './filter.scss';
 
 import '@comp/form-set/form-set'
 import '@comp/date-filter/date-filter'
+import '@comp/dropdown/dropdown'
