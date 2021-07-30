4c2b6f786b1af1bfb3e6c0810a9bcc2d8ffa161b
Igor Smolkov <smolkov.igor@gmail.com>
2021-07-30 15:05:32 +0700

    Дособрал фильтр

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -3,3 +3,6 @@ import './filter.scss';
 import '@comp/form-set/form-set'
 import '@comp/date-filter/date-filter'
 import '@comp/dropdown/dropdown'
+import '@comp/slider/slider'
+import '@comp/check-button/check-button'
+import '@comp/expandable-list/expandable-list'
