8693ac70da8afab23abf2116a2c8f9a8ea4748ab
Igor Smolkov <smolkov.igor@gmail.com>
2021-07-31 18:26:00 +0700

    Заполнил room-details компонентами

--- src/pages/website/room-details/room-details.js
+++ src/pages/website/room-details/room-details.js
@@ -3,3 +3,9 @@ import './room-details.scss';
 import '@comp/header/header';
 import '@comp/footer/footer';
 import '@comp/room-photos/room-photos'
+import '@comp/room-section/room-section'
+import '@comp/extra-list/extra-list'
+import '@comp/comment/comment'
+import '@comp/bullet-list/bullet-list'
+import '@comp/card/card'
+import '@comp/card-form/card-form'
