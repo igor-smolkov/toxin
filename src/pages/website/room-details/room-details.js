9c69d9fb08b0e5aafd62bc0cfde8499658fcd0d1
Igor Smolkov <smolkov.igor@gmail.com>
2021-07-31 20:49:20 +0700

    Отрефакторил диаграмму

--- src/pages/website/room-details/room-details.js
+++ src/pages/website/room-details/room-details.js
@@ -5,6 +5,7 @@ import '@comp/footer/footer';
 import '@comp/room-photos/room-photos'
 import '@comp/room-section/room-section'
 import '@comp/extra-list/extra-list'
+import '@comp/chart/chart'
 import '@comp/comment/comment'
 import '@comp/bullet-list/bullet-list'
 import '@comp/card/card'
