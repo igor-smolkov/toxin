371f079be35eca30d4473bc7e59c6ed71ab9a078
Igor Smolkov <smolkov.igor@gmail.com>
2021-08-01 16:48:58 +0700

    Отрефакторил вебпак конфиг

--- webpack.config.js
+++ webpack.config.js
@@ -1,193 +1,188 @@
-const path = require('path')
-const webpack = require('webpack')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserWebpackPlugin = require('terser-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
-//определение режима сборки
-const isDev = process.env.NODE_ENV === 'development'
-const isProd = !isDev
+// определение режима сборки
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
-//имена файлов
-const entryPoint = 'script.js'
-const baseTemplate = 'index.pug'
-const outputHTML = 'index.html'
-
-const imagesDir = 'images'
-const fontsDir = 'fonts'
-
-const pagesDir = 'pages'
+// имена файлов
+const imagesDir = 'images';
+const fontsDir = 'fonts';
+const pagesDir = 'pages';
 const pages = [
-    { 'ui-kit' : ['colors-and-type', 'form-elements', 'cards', 'headers-and-footers'] }, 
-    {
-        'website' : [
-            'landing-page', 'landing-page-dates-dropdown', 'landing-page-guests-dropdown', 
-            'search-room', 'search-room-dropdown', 'search-room-checks',
-            'room-details', 'registration', 'sign-in'
-        ] 
-    }
-]
+  { 'ui-kit': ['colors-and-type', 'form-elements', 'cards', 'headers-and-footers'] },
+  {
+    website: [
+      'landing-page', 'landing-page-dates-dropdown', 'landing-page-guests-dropdown',
+      'search-room', 'search-room-dropdown', 'search-room-checks',
+      'room-details', 'registration', 'sign-in',
+    ],
+  },
+];
 
-//сборка имен
-const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+// сборка имен
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
 
-//оптимизация
+// оптимизация
 const optimization = () => {
-    const config = {
-        splitChunks: {
-            chunks: 'all'
-        }
-    }
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
 
-    if (isProd) {
-        config.minimizer = [
-            new OptimizeCssAssetsWebpackPlugin(),
-            new TerserWebpackPlugin()
-        ]
-    }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
 
-    return config
-}
+  return config;
+};
 
-//добавление лоадеров css и его препроцессоров
-const cssLoaders = extra => {
-    const loaders = [
-        {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-                publicPath: ''
-                // hmr: isDev,
-                // reloadAll: true
-            }
-        }, 
-        'css-loader'
-    ]
+// добавление лоадеров css и его препроцессоров
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        publicPath: '',
+      },
+    },
+    'css-loader',
+  ];
 
-    if (extra) {
-        loaders.push(extra)
-    }
+  if (extra) {
+    loaders.push(extra);
+  }
 
-    return loaders
-}
+  return loaders;
+};
 
+// формирование списка плагинов
 const plugins = () => {
-    const list = [
-        toHTMLPage(baseTemplate, outputHTML),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [ { from: path.resolve(__dirname, `src/${imagesDir}/`), to: imagesDir } ]
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-          }),
-        new MiniCssExtractPlugin({
-            filename: filename('css') //имя выходного css файла
-        })
-    ]
+  const packHTMLWebpackPlugin = (input, output, chunk) => new HTMLWebpackPlugin({
+    template: `./${input}`,
+    filename: output,
+    minify: {
+      collapseWhitespace: isProd,
+    },
+    chunks: ['main', chunk],
+  });
 
-    pages.forEach(type => {
-        const key = Object.keys(type)[0]
-        type[key].forEach(page => list.push(
-            toHTMLPage(`${pagesDir}/${key}/${page}/${page}.pug`, `${key}/${page}.html`, page)
-        ))
-    })
+  const list = [
+    packHTMLWebpackPlugin('index.pug', 'index.html'),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, `src/${imagesDir}/`), to: imagesDir }],
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ];
 
-    function toHTMLPage (input, output, chunk) {
-        return new HTMLWebpackPlugin({
-            template: `./${input}`,
-            filename: output,
-            minify: {
-                collapseWhitespace: isProd
-            },
-            chunks: ['main', chunk]
-        })
-    }
+  pages.forEach((type) => {
+    const key = Object.keys(type)[0];
+    type[key].forEach((page) => list.push(
+      packHTMLWebpackPlugin(
+        `${pagesDir}/${key}/${page}/${page}.pug`,
+        `${key}/${page}.html`,
+        page,
+      ),
+    ));
+  });
 
-    return list
-}
+  return list;
+};
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'), //папка исходников
-    entry: {
-        main: `./${entryPoint}`,
-        ['landing-page']: './pages/website/landing-page/landing-page.js',
-        ['landing-page-dates-dropdown']: './pages/website/landing-page/landing-page.js',
-        ['landing-page-guests-dropdown']: './pages/website/landing-page/landing-page.js',
-        ['search-room']: './pages/website/search-room/search-room.js',
-        ['search-room-dropdown']: './pages/website/search-room/search-room.js',
-        ['search-room-checks']: './pages/website/search-room/search-room.js',
-        ['room-details']: './pages/website/room-details/room-details.js',
-        ['registration']: './pages/website/registration/registration',
-        ['sign-in']: './pages/website/sign-in/sign-in',
-        ['colors-and-type']: './pages/ui-kit/colors-and-type/colors-and-type.js',
-        ['form-elements']: './pages/ui-kit/form-elements/form-elements.js',
-        ['cards']: './pages/ui-kit/cards/cards.js',
-        ['headers-and-footers']: './pages/ui-kit/headers-and-footers/headers-and-footers.js',
-    }, //входная точка сборки
-    output: {
-        filename: filename('js'), //выходной файл приложения
-        path: path.resolve(__dirname, 'dist') //папка сборки
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './script.js',
+    'landing-page': './pages/website/landing-page/landing-page.js',
+    'landing-page-dates-dropdown': './pages/website/landing-page/landing-page.js',
+    'landing-page-guests-dropdown': './pages/website/landing-page/landing-page.js',
+    'search-room': './pages/website/search-room/search-room.js',
+    'search-room-dropdown': './pages/website/search-room/search-room.js',
+    'search-room-checks': './pages/website/search-room/search-room.js',
+    'room-details': './pages/website/room-details/room-details.js',
+    registration: './pages/website/registration/registration',
+    'sign-in': './pages/website/sign-in/sign-in',
+    'colors-and-type': './pages/ui-kit/colors-and-type/colors-and-type.js',
+    'form-elements': './pages/ui-kit/form-elements/form-elements.js',
+    cards: './pages/ui-kit/cards/cards.js',
+    'headers-and-footers': './pages/ui-kit/headers-and-footers/headers-and-footers.js',
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@comp': path.resolve(__dirname, 'src/components'), // алиас на компоненты
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'), //алиас на папку исходников
-            '@comp': path.resolve(__dirname, 'src/components'), //алиас на компоненты
-        }
-    },
-    optimization: optimization(),
-    devServer: {
-        port: 4200,
-        hot: isDev,
-        open: true
-    },
-    devtool: isDev ? 'source-map' : false,
-    plugins: plugins(),
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: cssLoaders()
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: cssLoaders('sass-loader')
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                exclude: /fonts/,
-                loader: 'file-loader',
-                options: {
-                  name: `${imagesDir}/${filename('[ext]')}`,
-                }
-            },
-            {
-                test: /\.(ttf|woff|otf|eot|svg|woff2)$/,
-                loader: 'file-loader',
-                options: {
-                  name: `${fontsDir}/${filename('[ext]')}`,
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader'
-            }
-        ]
-    }
-}
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: isDev,
+    open: true,
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader'),
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        exclude: /fonts/,
+        loader: 'file-loader',
+        options: {
+          name: `${imagesDir}/${filename('[ext]')}`,
+        },
+      },
+      {
+        test: /\.(ttf|woff|otf|eot|svg|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: `${fontsDir}/${filename('[ext]')}`,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+    ],
+  },
+};
