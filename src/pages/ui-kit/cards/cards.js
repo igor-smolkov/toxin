5922c19f34a6ff58d87958097b906fa517f3b271
Igor Smolkov <smolkov.igor@gmail.com>
2021-08-26 03:43:05 +0700

    Перезаполнил и поправил ui-kit

--- src/pages/ui-kit/cards/cards.js
+++ src/pages/ui-kit/cards/cards.js
@@ -1,8 +1,11 @@
 import './cards.scss';
+import $ from 'jquery';
 
 import '../../../components/logo/logo';
 import '../../../components/card/card';
 import '../../../components/card-title/card-title';
 import '../../../components/card-form/card-form';
-import '../../../components/calendar/calendar';
-import '../../../components/preview/preview';
+import '../../../components/room/room';
+import Calendar from '../../../components/calendar/calendar';
+
+((elem) => new Calendar(elem))($('.js-calendar-unit').find('.calendar'));
--- src/pages/ui-kit/form-elements/field-elements/field-elements.js
+++ src/pages/ui-kit/form-elements/field-elements/field-elements.js
@@ -5,3 +5,5 @@ import '../../../../components/form-set/form-set';
 
 import '../../../../components/text-field/text-field';
 import '../../../../components/dropdown/dropdown';
+import '../../../../components/date-section/date-section';
+import '../../../../components/date-filter/date-filter';
