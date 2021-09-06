20dcae6b31d9f8f4989c64925faa8dffa0e28931
Igor Smolkov <smolkov.igor@gmail.com>
2021-09-06 22:18:42 +0700

    Подключил и настроил свой плагин слайдера

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,20 +1,17 @@
 import $ from 'jquery';
-import 'jquery-ui/ui/widgets/slider';
+import myJQuerySliderFactory from '../../plugins/jquery.my-jquery-slider';
 import './slider.scss';
 
+myJQuerySliderFactory($);
+
 $('.js-slider__root').each(function initSlider() {
-  const values = this.dataset.values
+  const limits = this.dataset.values
     .substring(1, this.dataset.values.length - 1)
-    .split(',');
-  const config = {
-    min: +values[0],
-    max: +values[values.length - 1],
-    values: [+values[1], +values[values.length - 2]],
-  };
-  $(this).slider({
-    range: true,
-    min: config.min,
-    max: config.max,
-    values: config.values,
+    .split(',')
+    .map((v) => +v);
+  $(this).myJQuerySlider({
+    limits,
+    step: 0.1,
+    withIndent: false,
   });
 });
