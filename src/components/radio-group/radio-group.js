3afd5a8de6d5c5e1bcf0fd26bfd2f06e89f564a3
Igor Smolkov <smolkov.igor@gmail.com>
2021-11-05 20:19:27 +0700

    Added enter control to toggle & radio, added tab control to all radio #21

---
+++ src/components/radio-group/radio-group.js
@@ -0,0 +1,42 @@
+import $ from 'jquery';
+
+class RadioGroup {
+  constructor($elem) {
+    this._$elem = $elem;
+    this._init();
+  }
+
+  _init() {
+    this._$radios = this._$elem.find('.js-radio-group__box');
+    this._bindEventListeners();
+  }
+
+  _bindEventListeners() {
+    this._$elem.on('focus', this._handleGroupFocus.bind(this));
+    this._$radios.each((_, radio) => radio.addEventListener('keydown', this._handleRadioKeyDown.bind(this)));
+  }
+
+  _handleGroupFocus() {
+    this._$radios[0].focus();
+  }
+
+  _handleRadioKeyDown(e) {
+    if (e.key === 'Tab') {
+      const index = Array.from(this._$radios).findIndex((radio) => radio === e.target);
+      const next = this._nextRadio(index);
+      if (!next) return;
+      next.focus();
+      e.preventDefault();
+    }
+    if (e.key === 'Enter') {
+      e.target.checked = true;
+      e.preventDefault();
+    }
+  }
+
+  _nextRadio(index) {
+    return index < this._$radios.length - 1 ? this._$radios[index + 1] : null;
+  }
+}
+
+$('.js-radio-group').each((_, elem) => new RadioGroup($(elem)));
---
+++ src/components/toggle/toggle.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+
+class Toggle {
+  constructor($elem) {
+    this._$elem = $elem;
+    this._init();
+  }
+
+  _toggle() {
+    this._$check.prop('checked', !this._$check.is(':checked'));
+  }
+
+  _init() {
+    this._$check = this._$elem.find('.js-toggle__box');
+    this._bindEventListeners();
+  }
+
+  _bindEventListeners() {
+    this._$check.on('keydown', this._handleKeyDown.bind(this));
+  }
+
+  _handleKeyDown(e) {
+    if (e.key !== 'Tab' && e.key !== ' ') e.preventDefault();
+    if (e.key === 'Enter') this._toggle();
+  }
+}
+
+$('.js-toggle').each((_, elem) => new Toggle($(elem)));
