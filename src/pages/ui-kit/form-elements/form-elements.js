423a6ccc1c586aae18cdca5b8e3f010f3234ac2e
Igor Smolkov <smolkov.igor@gmail.com>
2021-12-20 17:04:16 +0700

    Moved local sections to components #32

--- src/pages/ui-kit/form-elements/form-elements.js
+++ src/pages/ui-kit/form-elements/form-elements.js
@@ -6,6 +6,6 @@ import '../../../layouts/ui-kit/*.scss';
 import '../../../libs/**/*.scss';
 import '../../../components/**/*.scss';
 import '../../../components/**/*.js';
-import './**/*.scss';
+import './*.scss';
 
 ((elem) => new DateSection(elem))($('.js-date-section'));
--- src/pages/website/room-details/room-details.js
+++ src/pages/website/room-details/room-details.js
@@ -2,4 +2,4 @@ import '../../../layouts/website/*.scss';
 import '../../../libs/**/*.scss';
 import '../../../components/**/*.scss';
 import '../../../components/**/*.js';
-import './**/*.scss';
+import './*.scss';
